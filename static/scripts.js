6e33f98240623d67d4babc360410c92a13799e5b
jonnymortemore <jonathanmortemore@gmail.com>
2024-05-29 17:41:35 +1000

    added in music content

--- static/scripts.js
+++ static/scripts.js
@@ -1,8 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    document.querySelector('#jvd_games').style.display = "none"
-    document.querySelector('#web_design').style.display = "none"
-    document.querySelector('#music').style.display = "none"
 
     document.querySelector('nav').querySelectorAll('button').forEach((el) => {
         el.addEventListener('click', (e) => hide_elements(e.target.id));
@@ -14,10 +11,10 @@ document.addEventListener('DOMContentLoaded', function() {
 function hide_elements(id) {
     document.querySelectorAll('.section').forEach((el) => {
         if (id.includes(el.id)) {
-            el.style.display = ""
+            el.hidden = false;
         }
         else {
-            el.style.display = "none"
+            el.hidden = true;
         }
     }
 
